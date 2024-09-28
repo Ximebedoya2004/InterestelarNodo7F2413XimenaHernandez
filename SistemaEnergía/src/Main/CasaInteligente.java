4ba20aa40d310498801b2f08fcc8f49301de5718
Ximebedoya2004 <111651556+Ximebedoya2004@users.noreply.github.com>
2024-09-28 18:41:10 +0200

    Delete SistemaEnergía directory

--- SistemaEnergía/src/Main/CasaInteligente.java
+++
@@ -1,9 +0,0 @@
-package Main;
-
-public class CasaInteligente {
-
-    public static void main(String[] args) {
-        GestorDeDispositivos gestor = new GestorDeDispositivos();
-        gestor.calcularConsumoMensual();
-    }
-}
